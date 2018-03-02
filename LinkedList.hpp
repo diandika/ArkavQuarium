dfa0e78b0618d608e7994c6e6721e8858d4775bc
diandika <13515044@std.stei.itb.ac.id>
2018-03-02 10:53:01 +0700

    add linked list + documentation

---
+++ LinkedList.hpp
@@ -0,0 +1,19 @@
+#ifndef LINKEDLIST_HPP
+#define LINKEDLIST_HPP
+
+template <class T>
+class LinkedList {
+private:
+  /* data */
+
+public:
+
+
+  int find(T);
+  bool isEmpty();
+  void add(T);
+  void remove(T);
+  T getIndex(int);
+};
+
+#endif
